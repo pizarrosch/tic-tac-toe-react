3563e179be8ba9b02ce8145f9ac7483de9e0799d
pizarrosch <bayenrshc89@gmail.com>
2024-09-02 19:35:13 +0200

    Create a name edit function

---
+++ src/components/Input.jsx
@@ -0,0 +1,6 @@
+export default function Input ({value, onChange}) {
+
+  return (
+    <input value={value} onChange={onChange}/>
+  )
+}
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,26 @@
+import {useState} from "react";
+import Input from "./Input.jsx";
+
 export default function Player({children, symbol}) {
+
+  const [newName, setNewName] = useState(children);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEdit () {
+    setIsEditing(prevEditState => !prevEditState);
+  }
+
+  function onInputChange (e) {
+    setNewName(e.target.value);
+  }
+
   return (
     <li>
       <span className='player'>
-        <span className='player-name'>{children}</span>
+        {isEditing ? <Input value={newName} onChange={onInputChange}/> : <span className='player-name'>{newName}</span>}
         <span className='player-symbol'>{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEdit}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   )
 }
